fbedc22f3ec40df85e7e8419b2f04ab02e4cdef5
Willie <shibelgut.dev@gmail.com>
2020-12-23 13:53:32 +0600

    First  Lesson

--- js/script.js
+++ js/script.js
@@ -1,46 +1,40 @@
-'use strict';
+/* Задание на урок:
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+Проверить, чтобы все работало без ошибок в консоли */
 
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
 };
 
-console.log(numberOfFilms);
+const a = +prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?'),
+      c = +prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = [b];
+personalMovieDB.movies[c] = [d];
 
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
-
-if (personalMovieDB.count < 10) {
-   console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман!');
-} else {
-    console.log('Произошла ошибка');
-}
-
-
-
-
-
-
-
-// console.log(personalMovieDB);
+//////////////////////////////////////////////////////////////////////////////////////////////
