b6ea76117f48cf26be0dabf1040468f89df52f6c
Willie <shibelgut.dev@gmail.com>
2020-12-27 21:01:15 +0600

    add function

---
+++ js/lessons.js
@@ -0,0 +1,17 @@
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       return confirm('Родители разрешили?');
+//     }
+//   }
+
+function min (a,b) {
+    if (a < b) {
+        return (a);
+    } else {
+        return (b);
+    }
+}
+
+alert(min(9,1));
